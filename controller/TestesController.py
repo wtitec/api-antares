b16a808ec85e390ed5fcaf2cb8124e0a628b93ee
Willian Takashi Ishida <wti.designer@gmail.com>
2021-08-20 03:17:56 +0000

    JSON Web Token

--- controller/TestesController.py
+++ controller/TestesController.py
@@ -1,16 +1,37 @@
 import json
 from flask import request, Response
+from flask.json import jsonify
+from system.jwt.JwtSystem import JwtSystem
+
 
 
 class TestesController:
 
     def __init__(self, app):
+        ###
+        # POST - /login
+        ###
+        @app.route('/login', methods=['POST'])
+        def token():
+            try:
+                auth = request.authorization
+
+                if auth.username == 'test' and auth.password == 'test':
+                    token = JwtSystem.create_token(auth.username)
+                    return jsonify({'token': token})
+                return Response({}, status=200, mimetype='application/json')
+
+            except Exception as e:
+                resp = json.dumps({"error": str(e)})
+                return Response(resp, status=400, mimetype='application/json')
 
         ###
         # POST - /json
         ###
         @app.route('/json', methods=['POST'])
+        @JwtSystem.token_required
         def index():
+            
             try:
                 content = request.get_json()
                 resp = json.dumps({
---
+++ system/jwt/JwtSystem.py
@@ -0,0 +1,34 @@
+from flask import request, jsonify
+import jwt
+import datetime
+from functools import wraps
+
+SECRET_KEY = "118972de9c42c227d41bf8b6a8c0a31b74418b24951b1e7c8c8ca09ccd1c1301"
+
+class JwtSystem:
+   
+    def token_required(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+
+            token = None
+
+            if 'Authorization' in request.headers:
+                token = request.headers['Authorization'].replace(
+                    'Bearer ', '')
+
+            if not token:
+                return jsonify({'message': 'a valid token is missing'}),400
+
+            try:
+                data = jwt.decode(
+                    token, SECRET_KEY, algorithms=["HS256"])
+            except:
+                return jsonify({'message': 'token is invalid'}),401
+
+            return f(*args, **kwargs)
+        return decorator
+
+    def create_token(username):
+        time_expire = 60*60*24
+        return jwt.encode({'user': username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=time_expire)}, SECRET_KEY, algorithm="HS256")
