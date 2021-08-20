0c2d2173fc3fc7f997fec1c72d39c0398451149c
Willian Takashi Ishida <wti.designer@gmail.com>
2021-08-20 03:16:02 +0000

    InformationController

---
+++ controller/InformationController.py
@@ -0,0 +1,19 @@
+import json
+from flask import request, Response, jsonify
+
+class InformationController:
+
+    def __init__(self, app):
+
+        @app.route('/api-information', methods=['GET'])
+        def ApiInformation():
+            try:
+                resp = json.dumps({
+                    "code": 0,
+                    "dev": 'Willian Takashi Ishida',
+                    "version": '1.0.0'
+                })
+                return Response(resp, status=200, mimetype='application/json')
+            except Exception as e:
+                resp = json.dumps({"error": str(e)})
+                return Response(resp, status=400, mimetype='application/json')
--- route.py
+++ route.py
@@ -1,7 +1,10 @@
+from controller.InformationController import InformationController
 from controller.TestesController import TestesController
+import controller
 
 
 class Route:
 
     def __init__(self, app):
         TestesController(app)
+        InformationController(app)
