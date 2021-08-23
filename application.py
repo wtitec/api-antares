88c47a89931919a06275c463b5a8b2fa21d2efb7
Willian Takashi Ishida <wti.designer@gmail.com>
2021-08-23 16:57:22 +0000

    Add Cors

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from route import Route
+from flask_cors import CORS
 
 application = Flask(__name__)
+
+CORS(application)
+
 Route(application)
 if __name__ == '__main__':
     application.run()
