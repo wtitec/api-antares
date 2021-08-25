25da90650b6c718dfcfd8699aa949b9deb99767f
Willian Takashi Ishida <wti.designer@gmail.com>
2021-08-25 07:39:33 +0000

    MongoDB Config

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from route import Route
 from flask_cors import CORS
 
 application = Flask(__name__)
-
+application.config["MONGO_URI"] = "mongodb://aldebaran:accenturechallange_1qazxsw2@18.228.225.100:27017/accenture_challenge?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 CORS(application)
 
 Route(application)
